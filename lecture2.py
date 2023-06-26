25da9a09e06432488d0db7e91196692807cfa0f5
Surabhi25 <112242601+Surabhi-25@users.noreply.github.com>
2023-06-26 23:13:22 +0530

    Add files via upload

---
+++ lecture2.py
@@ -0,0 +1,48 @@
+a=10
+b=20
+sum=a+b
+sub=b-a
+mul=a*b
+div=b/a
+print("addition is:", sum)
+print("subtraction is:", sub)
+print("multiplication is:", mul)
+print("division is:", div)
+a+=10 #a=a+10=20
+b-=5 #b=b-5=15
+print("a+=10 is",a)
+print("b-=5 is",b)
+a*=5 #a=a*5=100
+b/=3 #b=b/3=5.0
+print("a*=5 is",a)
+print("b/=3 is",b)
+a%=5 #a=a%5=0
+b//=5 #b=b//3=5
+print("a%=5 is",a)
+print("b//=3 is",b)
+c=20
+d=30
+print(" is c>d ?",c>d) #false
+print(" is c<d ?",c<d) #true
+print(" is c==d ?",c==d) #false
+print(" is c!=d ?",c!=d) #true
+x=True
+y=False
+print("x and y is ", x and y)
+print("x or y is ", x or y)
+print("not x is", not x)
+
+m=7
+n=5
+print("m bitwise or n is",m|n)
+print("m bitwise and n is",m&n)
+print("m bitwise xor n is",m^n)
+a=3
+b=2
+print("a>>b is",a>>b)
+print("a<<b is", a<<b)
+print(a is b)
+print(a is not b)
+s="string"
+print("s" in s)
+print("x" in s)
